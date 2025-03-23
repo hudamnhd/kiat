df5a403cb2fc236f9ef94210b31fde9e5a930f8d
hudamnhd <muhamadhuda519@gmail.com>
2025-03-23 11:05:15 +0700

    update manifetst

--- vite.config.ts
+++ vite.config.ts
@@ -1,88 +1,88 @@
-import { defineConfig, PluginOption } from "vite";
-import react from "@vitejs/plugin-react";
+import replace from "@rollup/plugin-replace";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig, PluginOption } from "vite";
 import type { ManifestOptions, VitePWAOptions } from "vite-plugin-pwa";
 import { VitePWA } from "vite-plugin-pwa";
-import replace from "@rollup/plugin-replace";
 
 const pwaOptions: Partial<VitePWAOptions> = {
-	mode: "development",
-	base: "/",
-	includeAssets: ["fonts/*", "favicon/*"],
-	manifest: {
-		name: "Kiat",
-		short_name: "Kiat",
-		description: "Kiat membaca quran dan melakukan hal-hal keren",
-		theme_color: "#ffffff",
-		background_color: "#ffffff",
-		display: "standalone",
-		icons: [
-			{
-				src: "/favicon/web-app-manifest-192x192.png",
-				sizes: "192x192",
-				type: "image/png",
-				purpose: "maskable",
-			},
-			{
-				src: "/favicon/web-app-manifest-512x512.png",
-				sizes: "512x512",
-				type: "image/png",
-				purpose: "maskable",
-			},
-		],
-	},
-	devOptions: {
-		enabled: process.env.SW_DEV === "true",
-		/* when using generateSW the PWA plugin will switch to classic */
-		type: "module",
-		navigateFallback: "index.html",
-	},
+  mode: "development",
+  base: "/",
+  includeAssets: ["fonts/*", "favicon/*"],
+  manifest: {
+    name: "Kiat",
+    short_name: "Kiat",
+    description: "Kiat membaca quran dan melakukan hal-hal keren",
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "fullscreen",
+    icons: [
+      {
+        src: "/favicon/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/favicon/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  },
+  devOptions: {
+    enabled: process.env.SW_DEV === "true",
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: "module",
+    navigateFallback: "index.html",
+  },
 };
 const detailedDate = new Date().toLocaleString("id-ID", {
-	year: "numeric",
-	month: "2-digit",
-	day: "2-digit",
-	hour: "2-digit",
-	minute: "2-digit",
-	second: "2-digit",
-	hour12: false, // Gunakan format 24 jam
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hour12: false, // Gunakan format 24 jam
 });
 
 const replaceOptions = {
-	preventAssignment: true,
-	__DATE__: detailedDate,
+  preventAssignment: true,
+  __DATE__: detailedDate,
 };
 const claims = process.env.CLAIMS === "true";
 const reload = process.env.RELOAD_SW === "true";
 const selfDestroying = process.env.SW_DESTROY === "true";
 
 if (process.env.SW === "true") {
-	pwaOptions.srcDir = "src";
-	pwaOptions.filename = claims ? "claims-sw.ts" : "prompt-sw.ts";
-	pwaOptions.strategies = "injectManifest";
-	(pwaOptions.manifest as Partial<ManifestOptions>).name =
-		"PWA Inject Manifest";
-	(pwaOptions.manifest as Partial<ManifestOptions>).short_name = "PWA Inject";
-	pwaOptions.injectManifest = {
-		minify: false,
-		enableWorkboxModulesLogs: true,
-	};
+  pwaOptions.srcDir = "src";
+  pwaOptions.filename = claims ? "claims-sw.ts" : "prompt-sw.ts";
+  pwaOptions.strategies = "injectManifest";
+  (pwaOptions.manifest as Partial<ManifestOptions>).name =
+    "PWA Inject Manifest";
+  (pwaOptions.manifest as Partial<ManifestOptions>).short_name = "PWA Inject";
+  pwaOptions.injectManifest = {
+    minify: false,
+    enableWorkboxModulesLogs: true,
+  };
 }
 
 if (claims) pwaOptions.registerType = "autoUpdate";
 
 if (reload) {
-	// @ts-expect-error just ignore
-	replaceOptions.__RELOAD_SW__ = "true";
+  // @ts-expect-error just ignore
+  replaceOptions.__RELOAD_SW__ = "true";
 }
 
 if (selfDestroying) pwaOptions.selfDestroying = selfDestroying;
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		react(),
-		VitePWA(pwaOptions),
-		replace(replaceOptions) as PluginOption,
-	],
+  plugins: [
+    tailwindcss(),
+    react(),
+    VitePWA(pwaOptions),
+    replace(replaceOptions) as PluginOption,
+  ],
 });
